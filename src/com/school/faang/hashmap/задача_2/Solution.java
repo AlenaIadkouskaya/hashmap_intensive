20cb6ade0354e8a2efba1de7bf8694311bb25fbe
Alena Iadkouskaya <eiodkovskaya6@gmail.com>
2025-06-24 22:13:08 +0200

    solution of task number 2 from hashMapIntensive (review)

--- src/com/school/faang/hashmap/задача_2/Solution.java
+++ src/com/school/faang/hashmap/задача_2/Solution.java
@@ -14,17 +14,22 @@ public class Solution {
         example.addUser("petrov", new User("petrov", LocalDateTime.now(), "petrov"));
         example.addUser("af_pp", new User("afonia", LocalDateTime.now().plusDays(1), "af_pp"));
         example.addUser("iiii", new User("ivanov ivan", LocalDateTime.now().minusDays(20), "iiii"));
+        example.addUser("iiii", new User("ivan ivanov", LocalDateTime.now().minusDays(20), "iiii"));
         example.printUsers(example);
     }
 
     public void printUsers(Solution example) {
         Map<String, User> db = example.users;
         for (Map.Entry<String, User> user : db.entrySet()) {
-            System.out.println("Пользователь @" + user.getKey() + " полное имя: " + user.getValue().getFullName());
+            System.out.println("User @" + user.getKey() + " full name: " + user.getValue().getFullName());
         }
     }
 
     public void addUser(String username, User user) {
-        users.put(username, user);
+        if (!users.containsKey(username)) {
+            users.put(username, user);
+        } else {
+            throw new IllegalStateException("User with username @" + username + " exists");
+        }
     }
 }
